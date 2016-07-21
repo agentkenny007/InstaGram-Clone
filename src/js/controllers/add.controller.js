ad33b45eb104634dce63753a7c26b561b03cb37b
Ikenna <ikennaugwuh@gmail.com>
2016-07-21 16:38:50 -0400

    Added a 'likes' property to the images.

--- src/js/controllers/add.controller.js
+++ src/js/controllers/add.controller.js
@@ -1,10 +1,10 @@
 export default function AddController($scope, $http, SERVER, $state){
     $scope.addImg = img => {
+        img.likes = 0;
         $http.post(SERVER.URL, img).then(resp => {
             $state.go('root.home');
         });
     };
-
 }
 
 AddController.$inject = ['$scope', '$http', 'SERVER', '$state'];
